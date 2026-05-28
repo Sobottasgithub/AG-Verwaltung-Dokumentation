433ee994a90518f32749bdca316b6b991ffdf097
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-05-28 20:30:16 +0200

    init: create structure

---
+++ compile-to-pdf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+name="AG-Verwaltung-Dokumentation"
+
+if [[ -z "$1" ]]; then
+  name=$1
+fi
+
+if [ ! -d "./build" ]; then
+  echo "./build not found! Creating folder..."
+  mkdir build
+fi
+
+latexmk -pdf -shell-escape -pdflatex="pdflatex -interaction=nonstopmode" -jobname=$name -outdir=build main.tex
---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "AG Verwaltung Dokumentation";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    let
+      projectName = "AG-Verwaltung-Dokumentation";
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          pkgsPackages = with pkgs; [
+            (texlive.combine {
+              inherit (texlive)
+                scheme-medium
+                geometry
+                fancyhdr
+                pgfplots
+                latexmk
+                ;
+            })
+          ];
+          packages =
+            with pkgs;
+            [
+              texlab
+              zathura
+              wmctrl
+            ]
+            ++ pkgsPackages;
+        in
+        {
+          devShells.default = pkgs.mkShell { buildInputs = packages; };
+        };
+      flake = { };
+    };
+}
